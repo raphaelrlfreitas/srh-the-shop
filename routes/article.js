a5d9ee00b00d7e34202ce56f8a5161889755d85d
Raphael Freitas <rrabbi@hotmail.com>
2020-06-09 18:35:34 +0200

    Class 09/06

--- routes/article.js
+++ routes/article.js
@@ -4,14 +4,28 @@ const db = require('../db/index');
 const router = express.Router();
 
 router.get('/article/:id', function(req, res, next) {
+  const id = req.params.id;
 
-  db.query(`select article_pk, description, price, caption from articles where article_pk = ${req.params.id}`, (err, result) => {
-    if(err){
-      next(err);
-    }
 
-    res.render('article', { article: result.rows[0] });
-  })
+  db
+    .query(`SELECT * FROM articles WHERE article_pk = ${id}`)
+    .then(data => {
+      
+      const article = data.rows[0];
+
+      db
+        .query(`SELECT * FROM ratings WHERE article_fk = ${id}`)
+        .then(data => {
+          
+          const ratings = data.rows;
+          res.render('article', { article, ratings })
+        })
+        .catch(err => console.log(err))
+
+    })
+    .catch(err => console.log(err))
+
+
 
 });
 
